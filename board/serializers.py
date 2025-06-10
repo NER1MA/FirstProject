3daa85d9f850a7080357365d4d5f07613102522f
방정훈 <nerima@MacBook-Air.local>
2025-06-10 13:51:12 +0900

    게시글/댓글 API 수정

--- board/serializers.py
+++ board/serializers.py
@@ -6,6 +6,20 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ['id', 'title', 'content', 'created_at']
 
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['post-title'] = rep.pop('title')
+        rep['post-content'] = rep.pop('content')
+        return rep
+
+    def to_internal_value(self, data):
+        data = data.copy()
+        if 'post-title' in data:
+            data['title'] = data.pop('post-title')
+        if 'post-content' in data:
+            data['content'] = data.pop('post-content')
+        return super().to_internal_value(data)
+
     def create(self, validated_data):
         request = self.context.get('request')
         if request and request.user and request.user.is_authenticated:
